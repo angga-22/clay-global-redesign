5b2b779485be718c41376cd7bc6b21fd7c79b839
angga-thetreedots <angga.saputra@thetreedots.com>
2022-11-23 09:51:44 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `clay global`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
